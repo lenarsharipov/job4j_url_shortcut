70214eb318ca6adef9b6550a4fac0a3f8d1a6038
lenarsharipov <lenarsharipov@gmail.com>
2023-06-01 00:59:20 +0300

    Upload validation groups marker interfaces

---
+++ src/main/java/ru/job4j/urlshortcut/validation/Operation.java
@@ -0,0 +1,20 @@
+package ru.job4j.urlshortcut.validation;
+
+/**
+ * Operation class includes marking interfaces that indicate
+ * whether field is validated or not per Create, Delete, Update operations.
+ */
+public class Operation {
+    public interface OnCreate {
+
+    }
+
+    public interface OnUpdate {
+
+    }
+
+    public interface OnDelete {
+
+    }
+
+}
