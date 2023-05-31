a930ef2c13960b74f76b0b0def4eba11d57a070e
lenarsharipov <lenarsharipov@gmail.com>
2023-06-01 01:01:28 +0300

    Upload Main.class

---
+++ src/main/java/ru/job4j/urlshortcut/handler/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package ru.job4j.urlshortcut.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Global exception handler ofNPE, IllegalArgumentException,
+ * and MethodArgumentNotValidException.
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    private final ObjectMapper objectMapper;
+
+    public GlobalExceptionHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class, IllegalArgumentException.class})
+    public void handleException(Exception e, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", "Some fields is empty");
+                put("details", e.getMessage());
+            }
+        }));
+        log.error(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handle(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(
+                e.getFieldErrors().stream()
+                        .map(f -> Map.of(
+                                f.getField(),
+                                String.format("%s. Actual value: %s",
+                                        f.getDefaultMessage(), f.getRejectedValue())
+                        ))
+                        .collect(Collectors.toList())
+        );
+    }
+}
